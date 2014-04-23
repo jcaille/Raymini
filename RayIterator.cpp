30638e104c91ca0690c7d9400f58356ebaea3e03
Florian Denis <me@floriandenis.net>
2014-04-23 10:25:22 +0200

    Created base RayIterator class

---
+++ RayIterator.cpp
@@ -0,0 +1,42 @@
+//
+//  RayIterator.cpp
+//  RayTracer
+//
+//  Created by Florian Denis on 23/04/14.
+//  Copyright (c) 2014 Florian Denis. All rights reserved.
+//
+
+#include "RayIterator.h"
+
+RayIterator::RayIterator(Vec3Df cameraPosition,
+                         Vec3Df direction,
+                         Vec3Df upVector,
+                         Vec3Df rightVector,
+                         float fov,
+                         float ar,
+                         unsigned int screenWidth,
+                         unsigned int screenHeight)
+{
+    _camPos = cameraPosition;
+    _direction = direction;
+    _upVector = upVector;
+    _rightVector = rightVector;
+    _fieldOfView = fov;
+    _aspectRatio = ar;
+    _screenHeight = screenHeight;
+    _screenWidth = screenWidth;
+}
+
+void RayIterator::raysForPixel(int i, int j, std::vector<Ray>& res)
+{
+    float tanX = tan (_fieldOfView)*_aspectRatio;
+    float tanY = tan (_fieldOfView);
+    Vec3Df stepX = ((float) i - _screenWidth/2.f)/(_screenWidth) * tanX * _rightVector;
+    Vec3Df stepY = ((float) j - _screenHeight/2.f)/(_screenHeight) * tanY * _upVector;
+    Vec3Df step = stepX + stepY;
+    Vec3Df dir = _direction + step;
+    dir.normalize ();
+
+    res.clear();
+    res.push_back(Ray(_camPos, dir));
+}
---
+++ RayIterator.h
@@ -0,0 +1,38 @@
+//
+//  RayIterator.h
+//  RayTracer
+//
+//  Created by Florian Denis on 23/04/14.
+//  Copyright (c) 2014 Florian Denis. All rights reserved.
+//
+
+#ifndef __RayTracer__RayIterator__
+#define __RayTracer__RayIterator__
+
+#include "Ray.h"
+#include "Vec3D.h"
+#include <vector>
+
+class RayIterator {
+ 
+    // informations about scene
+    Vec3Df _camPos;
+    Vec3Df _direction;
+    Vec3Df _upVector;
+    Vec3Df _rightVector;
+    float _fieldOfView;
+    float _aspectRatio;
+    unsigned int _screenWidth;
+    unsigned int _screenHeight;
+    int _idx;
+    
+    
+public:
+    
+    // Constructor
+    RayIterator(Vec3Df cameraPosition, Vec3Df direction, Vec3Df upVector, Vec3Df rightVector, float fov, float ar, unsigned int screenWidth, unsigned int screenHeight);
+    
+    void raysForPixel(int i, int j, std::vector<Ray>& res);
+};
+
+#endif /* defined(__RayTracer__RayIterator__) */
